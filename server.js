667aa5047207715f5cda018e473c79659dbe75cc
Vyas Brijesh <103355016+brijvyas-7@users.noreply.github.com>
2025-06-28 03:02:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,26 @@ const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
 const cron = require("node-cron");
-require('dotenv').config(); 
+require("dotenv\config");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Firebase Admin Setup
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
