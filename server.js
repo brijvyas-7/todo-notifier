c5d62099f95e027fd62927165e092f197bf51790
Vyas Brijesh <103355016+brijvyas-7@users.noreply.github.com>
2025-06-29 20:59:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -79,8 +79,9 @@ cron.schedule("* * * * *", async () => {
           body: JSON.stringify({
             app_id: process.env.ONESIGNAL_APP_ID,
             include_player_ids: [task.playerId],
-            headings: { en: "⏰ Reminder" },
+            headings: { en: "⏰ Reminder : Hey buddy!" },
             contents: { en: `Your task '${task.name}' is due now!` },
+            url: "https://brijvyas-7.github.io/Todo-List/"
           })
         });
 
